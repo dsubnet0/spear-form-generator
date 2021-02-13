4147ea6ac0f8d9fe1c71212c207cda4f3048e401
dsubnet0 <dsubnet0@gmail.com>
2021-02-13 13:26:47 -0500

    Fleshed out move list. Separate de-escalators

--- app.py
+++ app.py
@@ -2,13 +2,24 @@ from flask import Flask, render_template
 import random
 app = Flask('Spear Form Generator')
 
-ALL_MOVES = ['Hands on hips','Arms folded', 'Negotiator', 'Index palm']
+TROJAN_HORSE = ['Hands on hips','Arms folded', 'Negotiator', 'Half negotiator', 'Jack Benny']
+COMBATIVES = ['Index palm', 'Rear index palm', 'SPEAR stance',
+                'Full SPEAR', 'Rear up elbow','Front horizontal elbow', 
+                'Downward half-SPEAR', 'Punching knee', 'Jackhammer front kick',
+                'Downward full SPEAR', 'X-axis shin kick', 'Low SPEAR']
 
 def generate_random_move_list(number_of_moves=10):
     my_list = []
+    # Random number of de-escalation stances
+    for d in range(random.randint(0,4)):
+        next_move = ''
+        next_move = random.choice(TROJAN_HORSE)
+        if len(my_list)>0 and next_move == my_list[-1]: continue
+        my_list.append(next_move)
+        
     while len(my_list) < number_of_moves:
         next_move = ''
-        next_move = random.choice(ALL_MOVES)
+        next_move = random.choice(COMBATIVES)
         if len(my_list)>0 and next_move == my_list[-1]: continue
         my_list.append(next_move)
     return my_list
